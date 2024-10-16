6712d71a693616749f9b8c7063ec9e76229107b3
Seva SevaVl <stalker2002ddas@gmail.com>
2024-10-16 06:49:20 +0000

    kkl

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,54 @@
-<script>
-	let name = 'Seva'; //Задали переменную
-
-  let src = '/image.gif'; //Добавляем картинку
-</script>
-
-
-
-<h1>
+<style>
+  .flex-container {
   
-  Hello {name.toUpperCase()}!
+    display: flex;
+    
+    flex-flow: wrap;
+    flex-wrap: wrap; 
+  
+    background-image: url("181.jpg");
+    height: 100%;
+    padding: 15px;
+    gap: 5px;
+  
+  }
+  
+  .flex-container > div{
+    background: #e3b3ff;
+    border: 3px solid #80ccff;
+    border-radius: 5px;
+    padding: 8px;
+  }
+  
+  </style>
+  
+  <div class="flex-container">
+    <div class="item3">Опа</div>
+    <div class="item1">Штука 2</div>
+    <div class="item3">Ещё одна штука 3</div>
+  
+  </div>
+  <div class="flex-container">
+  <div class="item3">Опа</div>
+  <div class="item1">Штука 2</div>
+  <div class="item3">Ещё одна штука 3</div>
 
-</h1>
+</div>
+<div class="flex-container">
+  <div class="item3">ппывы</div>
+  <div class="item1">рокеокы 2</div>
+  <div class="item3">кфрокцкы</div>
 
+</div>
+<div class="flex-container">
+  <div class="item3"><img src = "whitecatbaby.jpg" alt="{"котенок"} котёнок." width="300" height="" /></div>
+  <div class="item1"><img src = "whitecatbaby.jpg" alt="{"котенок"} котёнок." width="300" height="" /></div>
+  <div class="item3"><img src = "whitecatbaby.jpg" alt="{"котенок"} котёнок." width="300" height="" /></div>
 
+</div>
+<div class="flex-container">
+  <div class="item3"><img src = "ittenVes170221.jpg" alt="{"котенок"} котёнок." width="300" height="" /></div>
+  <div class="item1"><img src = "ittenVes170221.jpg" alt="{"котенок"} котёнок." width="300" height="" /></div>
+  <div class="item3"><img src = "ittenVes170221.jpg" alt="{"котенок"} котёнок." width="300" height="" /></div>
 
-<img src = {src} alt = "A man dances(Вы зарикролены)"/>
-
-
-
-<p>Здесь могла бы быть ваша реклама</p>
-<style> /*задали текст и его параметры*/
-	p{
-		color:rgb(48, 173, 48);
-		front-family: 'Comic Sans MS', cursive;
-		font-size: 2em;
-	}
-</style>
-
-
-
+</div>
